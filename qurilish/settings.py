84329da681e7ee1cb1606bd84422310a8a55c4e2
BokijonovS <sanatjon.dark@gmail.com>
2024-06-05 09:02:39 +0500

    default authentication added

--- qurilish/settings.py
+++ qurilish/settings.py
@@ -128,5 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
